b176afbf99ce859c7adae7e6ad5d275ab7b0a33f
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2024-09-14 22:51:49 +0300

    Update download_last_spacex_launch.py

--- download_last_spacex_launch.py
+++ download_last_spacex_launch.py
@@ -23,9 +23,9 @@ def main():
 
     launch_id = os.getenv('LAST_LAUNCH_KEY')
 
-    url_nasa = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    nasa_url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
 
-    response = requests.get(url_nasa)
+    response = requests.get(nasa_url)
     response.raise_for_status()
     last_launch_links = response.json()['links']['flickr']['original']
 
