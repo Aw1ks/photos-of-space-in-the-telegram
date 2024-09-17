a75d220f3a2319960e2d11be3fefb5fd3ac49726
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2024-09-17 11:02:48 +0300

    Добавил защиту от ошибок в GET параметрах download_epic_nasa.py

--- download_epic_nasa.py
+++ download_epic_nasa.py
@@ -31,15 +31,20 @@ def main():
     response.raise_for_status()
     epic_images = response.json()
 
+    params = {
+        'api_key': epic_key
+    }
+
     for epic_image in epic_images[:10]:
         file_name = epic_image["image"]
         epic_image_date = epic_image["date"]
 
         epic_image_date = datetime.fromisoformat(epic_image_date).strftime("%Y/%m/%d")
-        link_path = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{file_name}.png?api_key={epic_key}"
+        link_path = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{file_name}.png"
+        link_path_with_params = f'{link_path}?{urlencode(params)}'
         path = os.path.join(epic_folder, f'{file_name}.png')
 
-        download_image(link_path, path)
+        download_image(link_path_with_params, path)
 
 
 if __name__ == "__main__":
