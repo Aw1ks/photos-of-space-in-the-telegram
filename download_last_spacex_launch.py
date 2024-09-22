4168264c82adf5a3210e3bb095a3458aab8554dd
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2024-09-22 12:52:13 +0300

    Update download_last_spacex_launch.py

--- download_last_spacex_launch.py
+++ download_last_spacex_launch.py
@@ -9,31 +9,31 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    parser = argparse.ArgumentParser(description='Это скрипт download_last_spacex_launch.py в переменную epic_key введите свай наса-ключ и запустите скрип: download_last_spacex_launch.py --folder (имя папки в которую хотите скачать фото)')
-
+    parser = argparse.ArgumentParser(description='Введите ID первого запуска:')
     parser.add_argument('--folder',
                         type=str,
                         default='last_spacex_launch_images',
                         help='Введите имя папки в которую хотите поместить фотографии:')
-
+    
     args = parser.parse_args()
-    name_folder_last_launch = args.folder
+    folder_last_launch = args.folder
 
-    os.makedirs(name_folder_last_launch, exist_ok=False)
+    if not os.path.exists(folder_last_launch):
+        os.makedirs(folder_last_launch)
 
     launch_id = os.getenv('LAST_LAUNCH_KEY')
 
-    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    url_nasa = f'https://api.spacexdata.com/v5/launches/{launch_id}'
 
-    response = requests.get(url)
+    response = requests.get(url_nasa)
     response.raise_for_status()
     last_launch_links = response.json()['links']['flickr']['original']
 
-    for url_number, last_nasa_url in enumerate(last_launch_links, start=1):
-        filename = f'spacex{url_number}.jpg'
-        full_path = os.path.join(name_folder_last_launch, filename)
+    for url_number, last_nasa_url in enumerate(last_launch_links):
+        filename = f'spacex{url_number+1}.jpg'
+        full_path = os.path.join(folder_last_launch, filename)
 
-        download_image(last_nasa_url, full_path)
+        download_image(folder_last_launch, last_nasa_url, full_path)
 
 
 if __name__ == "__main__":
