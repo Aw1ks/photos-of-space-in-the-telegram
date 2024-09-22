7a2528efba86ef616e911367193f58083168afc2
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2024-09-22 12:52:41 +0300

    Update save_images.py

--- save_images.py
+++ save_images.py
@@ -1,16 +1,20 @@
-import requests 
 import os
+from pathlib import Path
 
+import requests
 from urllib.parse import urlparse
 
 
-def download_image(url, full_path):
-
-    response = requests.get(url)
+def download_image(folder, url, filename, api_key=''):
+    
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url, params=api_key)
     response.raise_for_status()
-
+    full_path = os.path.join(folder, filename)
+    
     with open(full_path, 'wb') as file:
         file.write(response.content)
+
 
 def get_extansion(url):
     path = urlparse(url).path
